c1f92ce284ab14c3e4b0f5d01afba7db51fd732d
图片加密 <946585434@qq.com>
2018-06-07 10:28:15 +0000

    Create header.php

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+
+    <META HTTP-EQUIV="Pragma" CONTENT="no-cache"> 
+
+    <META HTTP-EQUIV="Cache-Control" CONTENT="no-cache"> 
+
+    <META HTTP-EQUIV="Expires" CONTENT="0">
+
+    <link rel="stylesheet" type="text/css" href="main.css">
+
+    <script src="http://apps.bdimg.com/libs/jquery/1.10.2/jquery.min.js"></script>
+
+    <script src="menu.js"></script>
