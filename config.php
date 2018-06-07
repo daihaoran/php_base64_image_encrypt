9b34a73934341015fcbbde23e1ff5c5cb14d3fcc
图片加密 <946585434@qq.com>
2018-06-07 11:44:45 +0000

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = images;  /*文件名*/
+
+$newExtension = apple;  /*扩展名*/
+
+$outFileOk = false;  /*是否允许写输出？*/
+
+$forceDeleteFile = false;  /*true暴力删除，false只留现在一张图片*/
+
+$fileName = $name . "." . $newExtension;
